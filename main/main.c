80ad649de0cc12e521acf20ae1ba571190e224c1
bdcabreran <45661967+bdcabreran@users.noreply.github.com>
2023-04-03 11:06:22 -0500

    feat (Removing Noise in main.c)

--- main/main.c
+++ main/main.c
@@ -23,66 +23,16 @@
 /* Littlevgl specific */
 #include "lvgl/lvgl.h"
 #include "lvgl_helpers.h"
-// #include "lvgl/examples\widgets/lv_example_widgets.h"
 
 // #include "ui.h"
 #include "ClockAlarmUI.h"
 #include "glvgl.h"
 #include "clock.h"
 
-static uint32_t millis_1, millis_2;
+
+
 UI_event_t event;
-
-#if 1
-static void event_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * obj = lv_event_get_current_target(e);
-
-    if(code == LV_EVENT_VALUE_CHANGED) {
-        lv_calendar_date_t date;
-        if(lv_calendar_get_pressed_date(obj, &date)) {
-            LV_LOG_USER("Clicked date: %02d.%02d.%d", date.day, date.month, date.year);
-        }
-    }
-}
-
-
-void lv_example_calendar_1(void)
-{
-    lv_obj_t  * calendar = lv_calendar_create(lv_scr_act());
-    lv_obj_set_size(calendar, 185, 185);
-    lv_obj_align(calendar, LV_ALIGN_CENTER, 0, 27);
-    lv_obj_add_event_cb(calendar, event_handler, LV_EVENT_ALL, NULL);
-
-    lv_calendar_set_today_date(calendar, 2021, 02, 23);
-    lv_calendar_set_showed_date(calendar, 2021, 02);
-
-    /*Highlight a few days*/
-    static lv_calendar_date_t highlighted_days[3];       /*Only its pointer will be saved so should be static*/
-    highlighted_days[0].year = 2021;
-    highlighted_days[0].month = 02;
-    highlighted_days[0].day = 6;
-
-    highlighted_days[1].year = 2021;
-    highlighted_days[1].month = 02;
-    highlighted_days[1].day = 11;
-
-    highlighted_days[2].year = 2022;
-    highlighted_days[2].month = 02;
-    highlighted_days[2].day = 22;
-
-    lv_calendar_set_highlighted_dates(calendar, highlighted_days, 3);
-
-#if LV_USE_CALENDAR_HEADER_DROPDOWN
-    lv_calendar_header_dropdown_create(calendar);
-#elif LV_USE_CALENDAR_HEADER_ARROW
-    lv_calendar_header_arrow_create(calendar);
-#endif
-    lv_calendar_set_showed_date(calendar, 2021, 10);
-}
-#endif 
-
+ClockAlarmUI ClockAlarmUI_inst;
 
 /*********************
  *      DEFINES
@@ -97,12 +47,6 @@ static void lv_tick_task(void *arg);
 void guiTask(void *pvParameter);
 
 
-void external_button_on_screen();
-void keyboard_on_chip();
-void encoder_on_chip();
-void encoder_on_chip_button();
-void task_with_rtos(void);
-void task_with_rtos_semaphore(void);
 /**********************
  *   APPLICATION MAIN
  **********************/
@@ -113,7 +57,7 @@ void app_main() {
     xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
 }
 
-static void lv_alarm_task(void *arg)
+static void lv_alarm_timer_handler(void *arg)
 {
     (void)arg;
 
@@ -129,45 +73,9 @@ static void lv_alarm_task(void *arg)
 static void lv_tick_task(void *arg)
 {
     (void)arg;
-
     lv_tick_inc(LV_TICK_PERIOD_MS);
 
 }
-
-static void drag_event_handler(lv_event_t * e)
-{
-    lv_obj_t * obj = lv_event_get_target(e);
-
-    lv_indev_t * indev = lv_indev_get_act();
-    if(indev == NULL)  return;
-
-    lv_point_t vect;
-    lv_indev_get_vect(indev, &vect);
-
-    lv_coord_t x = lv_obj_get_x(obj) + vect.x;
-    lv_coord_t y = lv_obj_get_y(obj) + vect.y;
-    lv_obj_set_pos(obj, x, y);
-}
-
-
-/**
- * Make an object dragable.
- */
-void lv_example_obj_2(void)
-{
-    lv_obj_t * obj;
-    obj = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(obj, 150, 100);
-    lv_obj_add_event_cb(obj, drag_event_handler, LV_EVENT_PRESSING, NULL);
-
-    lv_obj_t * label = lv_label_create(obj);
-    lv_label_set_text(label, "Drag me");
-    lv_obj_center(label);
-
-}
-
-ClockAlarmUI ClockAlarmUI_inst;
-
 
 //Creates a semaphore to handle concurrent call to lvgl stuff
 //If you wish to call *any* lvgl function from other threads/tasks
@@ -184,44 +92,24 @@ void guiTask(void *pvParameter) {
     /* Initialize SPI or I2C bus used by the drivers */
     lvgl_driver_init();
 
-
     static lv_color_t buf1[DISP_BUF_SIZE];
-#ifndef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
     static lv_color_t buf2[DISP_BUF_SIZE];
-#endif
     static lv_disp_draw_buf_t disp_buf;
-
     uint32_t size_in_px = DISP_BUF_SIZE;
-
-#if defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_IL3820 
-    /* Actual size in pixel, not bytes and use single buffer */
-    size_in_px *= 8;
-    lv_disp_buf_init(&disp_buf, buf1, NULL, size_in_px);
-#elif defined CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
-    lv_disp_buf_init(&disp_buf, buf1, NULL, size_in_px);
-#else
     lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
-#endif
 
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
     disp_drv.draw_buf = &disp_buf;
 
-#ifdef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
-    disp_drv.rounder_cb = disp_driver_rounder;
-    disp_drv.set_px_cb = disp_driver_set_px;
-#endif
-
     lv_disp_drv_register(&disp_drv);
 
-#if CONFIG_LVGL_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.read_cb = touch_driver_read;
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     lv_indev_drv_register(&indev_drv);
-#endif
 
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
@@ -231,50 +119,15 @@ void guiTask(void *pvParameter) {
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-    const esp_timer_create_args_t periodic_timer_args_2 = {
-        .callback = &lv_alarm_task,
-        .name = "alarm_clk"
+    const esp_timer_create_args_t periodic_alarm_timer_args = {
+        .callback = &lv_alarm_timer_handler,
+        .name = "alarm_timer"
     };
     esp_timer_handle_t alarm_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args_2, &alarm_timer));
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_alarm_timer_args, &alarm_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(alarm_timer, 1000 * 1000)); // every Second
 
-
-#if defined CONFIG_LVGL_TFT_DISPLAY_MONOCHROME || \
-    defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_ST7735S
-    
-    /* use a pretty small demo for monochrome displays */
-    /* Get the current screen  */
-    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
-
-    /*Create a Label on the currently active screen*/
-    lv_obj_t * label1 =  lv_label_create(scr, NULL);
-
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello\nworld!");
-
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-#else
-    // lv_demo_widgets();                                      
-    // external_button_on_screen();
-    // keyboard_on_chip();  
-//    encoder_on_chip_button();
-//    task_with_rtos();
-//    task_with_rtos_semaphore();
-        // lv_example_calendar_1();
-
-    // ui_init();
-    // rgb_mixer_create_ui();
-    // lv_example_obj_2();
-
-
     ClockAlarmUI_ctor(&ClockAlarmUI_inst);
-    
-#endif
-
     
     while (1) {
         vTaskDelay(1);
